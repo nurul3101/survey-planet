ea5fdf3104e75589e2a273c12d0e387450e39c90
Nurulraza Sundarani <nurulraza26@gmail.com>
2020-06-27 19:11:08 +0530

    Test Authentication

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,10 +1,17 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
+const serviceAccount = require('./survey-planet-app.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://survey-planet-app.firebaseio.com',
+})
 /* Initialise Firestore*/
-admin.initializeApp()
 
 const firestore = admin.firestore()
+const auth = admin.auth()
+
 firestore.settings({
   timestampsInSnapshots: true,
 })
@@ -14,5 +21,5 @@ const { signUpFunc } = require('./src/auth/signup')
 exports.signup = functions
   .runWith({ timeoutSeconds: 60, memory: '1GB' })
   .https.onRequest((request, response) => {
-    signUpFunc(request, response, firestore)
+    signUpFunc(request, response, firestore, auth)
   })
--- backend/functions/src/auth/signup.js
+++ backend/functions/src/auth/signup.js
@@ -1,9 +1,21 @@
-exports.signUpFunc = (req, res, firestore) => {
-  console.log(req.body)
-  firestore.collection('Users').add({
-    name: 'nurul',
-  })
-  res.status(200).send({
-    success: true,
-  })
+exports.signUpFunc = (req, res, firestore, auth) => {
+  auth
+    .createUser({
+      email: 'nurulraza26@gmail.com',
+      emailVerified: true,
+      password: 'test123',
+    })
+    .then((userRecord) => {
+      console.log('userRecord', userRecord)
+      firestore
+        .collection('Users')
+        .add({
+          name: 'nurul',
+        })
+        .then(() => {
+          res.status(200).send({
+            success: true,
+          })
+        })
+    })
 }
