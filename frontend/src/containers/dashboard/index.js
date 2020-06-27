7f29b093bd952b1bdb434338711be727cb413390
Nurulraza Sundarani <nurulraza26@gmail.com>
2020-06-27 17:21:31 +0530

    Added Dashboard Route

---
+++ frontend/src/containers/dashboard/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function dashboard() {
+  return <div>This is a dashboard</div>
+}
+
+export default dashboard
--- frontend/src/route.js
+++ frontend/src/route.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Signup from './containers/signup'
+import Dashboard from './containers/dashboard'
 
 export default () => {
   return (
     <Router>
       <Switch>
+        <Route path="/dashboard" component={Dashboard} />
         <Route path="/" component={Signup} />
       </Switch>
     </Router>
