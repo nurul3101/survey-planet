9cda59114c311271edf144b483b69b9634e3ef8f
Nurulraza Sundarani <nurulraza26@gmail.com>
2020-06-28 13:25:24 +0530

    promise converted into async await

--- backend/functions/src/auth/signup.js
+++ backend/functions/src/auth/signup.js
@@ -1,21 +1,32 @@
-exports.signUpFunc = (req, res, firestore, auth) => {
-  auth
-    .createUser({
-      email: 'nurulraza26@gmail.com',
+exports.signUpFunc = async (req, res, firestore, auth) => {
+  let reqObj = req.body
+  try {
+    const userRecord = await auth.createUser({
+      email: reqObj.email,
       emailVerified: true,
-      password: 'test123',
+      password: reqObj.password,
     })
-    .then((userRecord) => {
-      console.log('userRecord', userRecord)
-      firestore
-        .collection('Users')
-        .add({
-          name: 'nurul',
-        })
-        .then(() => {
-          res.status(200).send({
-            success: true,
-          })
-        })
+
+    console.log('userRecord', userRecord)
+
+    await firestore.collection('Users').add({
+      uid: userRecord.uid,
+      email: reqObj.email,
+      name: reqObj.name,
+      gender: reqObj.gender,
+      age: reqObj.age,
+      userType: reqObj.userType,
+      createdAt: new Date().getTime(),
     })
+
+    res.status(200).send({
+      success: true,
+    })
+  } catch (error) {
+    console.log('Error in creating user', error)
+    res.status(200).send({
+      success: false,
+      error,
+    })
+  }
 }
