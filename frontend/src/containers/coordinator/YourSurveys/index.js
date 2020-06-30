0ff68c7fdbb9f089dc59856e05d4695df62fc12b
Nurulraza Sundarani <nurulraza26@gmail.com>
2020-06-30 22:59:03 +0530

    Integrated Your Surveys Modal

--- frontend/src/containers/coordinator/YourSurveys/index.js
+++ frontend/src/containers/coordinator/YourSurveys/index.js
@@ -6,6 +6,12 @@ import { useSelector } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
 import { Typography, Divider } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import * as Survey from 'survey-react'
+import 'survey-react/survey.css'
 import configObj from '../../../Configuration'
 
 const useStyles = makeStyles((theme) => ({
@@ -33,6 +39,9 @@ function YourSurveys() {
 
   const [surveys, setSurveys] = useState([])
   const [showNoSurveysMessage, setShowNoSurveyMessage] = useState(false)
+  const [openSurveyDialog, setOpenSurveyDialog] = useState(false)
+
+  const [openedSurvey, setOpenedSurvey] = useState({})
 
   const fetchSurveys = async () => {
     try {
@@ -71,6 +80,15 @@ function YourSurveys() {
     fetchSurveys()
   }, [user])
 
+  const openSurveyInDialog = (e, survey) => {
+    console.log('survey', survey)
+    setOpenedSurvey(survey.surveyJSON)
+    setOpenSurveyDialog(true)
+  }
+
+  var model = new Survey.Model(openedSurvey)
+  model.mode = 'display'
+
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
@@ -100,7 +118,7 @@ function YourSurveys() {
                       variant="contained"
                       color="primary"
                       className={classes.button}
-                      onClick={() => {}}
+                      onClick={(e) => openSurveyInDialog(e, survey)}
                     >
                       View Survey
                     </Button>
@@ -110,6 +128,24 @@ function YourSurveys() {
             )
           })}
         </Paper>
+
+        <Dialog
+          open={openSurveyDialog}
+          onClose={() => setOpenSurveyDialog(false)}
+          aria-labelledby="form-dialog-title"
+          maxWidth={'md'}
+          fullWidth
+        >
+          <DialogTitle id="form-dialog-title">Survey</DialogTitle>
+          <DialogContent>
+            <Survey.Survey model={model} />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpenSurveyDialog(false)} color="primary">
+              Ok
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Grid>
     </Grid>
   )
